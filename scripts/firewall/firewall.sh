838bcfc34dd9eeab3f70634c44321e7ecbd43aec
grnt4v <39674579+grnt4v@users.noreply.github.com>
2018-06-10 22:37:01 +0700

    ad waiting time

--- scripts/firewall/firewall.sh
+++ scripts/firewall/firewall.sh
@@ -3,45 +3,30 @@ NC='\033[0m'
 GREEN='\033[0;32m'
 PURPLE='\033[0;35m'
 
-echo -e "
-Welcome to your firewall installation script for IPS Masternode.
+clear
 
-I will download the firewall rules for you, then install the firewall.
+echo -e "Welcome to your firewall installation script for IPS Masternode."
+
+sleep 2
+
+echo -e "
 You will be asked if you want the firewall to be started at boot time:
 ${RED}answer "no"${NC}
 
-I insist:
-${RED} _______________
-/   Answer no   \\
-\\_______________/${NC}
-Otherwise your masternode would get disconnected and you would have to
-wait rewards as if you had restarted your masternode.
-You definitely don't want this...
-
-No worry I will make your firewall start at boot time later.
-
-
-Also don't try to login more than 5 times in a 2 minutes row to your vps,
-you would be locked out for a short period of time.
-If you do however just be patient, you should be able to connect again in
-a few minutes...
-
-And now for something totally different!
-
-Enjoy!
+I insist
+${RED}Answer no to the following question${NC}
 "
 
-sleep 30
-
-echo -e 'Now let me download the firewall configuration file for ${PURPLE}IPS${NC}.'
-
+#echo -e 'Now let me download the firewall configuration file for ${PURPLE}IPS${NC}.'
 
 wget -O /tmp/ferm.conf https://raw.githubusercontent.com/grnt4v/guides/master/scripts/firewall/ferm.conf 2>/dev/null
-
+sleep 10
+clear
 echo -e "I'm installing your ${GREEN}firewall${NC}..."
 echo -e "Don't forget to answer ${RED}no${NC} to the next question!"
-echo -e "When ready press <enter>"
-sleep 15
+
+sleep 8
+
 apt-get update > /dev/null
 apt-get install -y ferm
 
