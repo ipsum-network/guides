e1a6ec1cbfab65eb3882972fb8fbd644eeee38cb
grnt4v <39674579+grnt4v@users.noreply.github.com>
2018-06-10 20:45:11 +0700

    testing 2

--- scripts/firewall/firewall.sh
+++ scripts/firewall/firewall.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-mkdir -p /etc/ferm
 
-wget -O /etc/ferm/ferm.conf https://raw.githubusercontent.com/grnt4v/guides/master/scripts/firewall/ferm.conf
+
+wget -O /tmp/ferm.conf https://raw.githubusercontent.com/grnt4v/guides/master/scripts/firewall/ferm.conf
 
 apt update
 apt install -y ferm
 
-# mv /tmp/ferm.conf /etc/ferm/ferm.conf
+mv /tmp/ferm.conf /etc/ferm/ferm.conf
 
 # ferm /etc/ferm/ferm.conf
